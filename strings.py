12b35f391a7f51309f174c94a0c6a08678e34d73
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-16 17:00:19 -0600

    added strings.py

---
+++ strings.py
@@ -0,0 +1,13 @@
+phrase = "Giraffe Academy"
+print("\"Giraffe Academy\"")
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase[3])
+print(phrase.index("G"))
+print(phrase.index("Academy"))
+print(phrase.replace("Giraffe", "Tiger"))
