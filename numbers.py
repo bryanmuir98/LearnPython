90ff79d8347681ba75a46a7a5d9b71e151e1d496
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-16 17:31:39 -0600

    added numbers.py

---
+++ numbers.py
@@ -0,0 +1,17 @@
+from math import *
+
+print(2)
+print(-2.0987)
+print(3 - 4.56)
+print((3 + 5) * 4)
+print(10 % 4)
+my_num = -5
+print(str(my_num) + " is my favorite number")
+print(abs(my_num))
+print(pow(2, abs(my_num)))
+print(max(2, abs(my_num)))
+print(round(3.49))
+print(floor(4.8))
+print(ceil(4.2))
+print(sqrt(64))
+
