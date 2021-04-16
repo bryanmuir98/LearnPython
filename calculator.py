305520b89b67af0e8226fd60357d613706aeb61f
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-16 17:50:01 -0600

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+num1 = input("Enter a number to add: ")
+num2 = input("Enter another number to add: ")
+result = float(num1) + float(num2)
+print("Your answer is " + str(result) + ".")
+
+#no type double in Python?
