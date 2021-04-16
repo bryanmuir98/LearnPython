f6063381ed87da37bca51e63f806655ca377807c
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-16 17:40:31 -0600

    added userInput.py

---
+++ userInput.py
@@ -0,0 +1,4 @@
+fName = input("Input your first name: ")
+lName = input("Input your last name: ")
+age = input("Enter your age: ")
+print("Hi there, " + fName + " " + lName + " you are " + age + " years old.")
