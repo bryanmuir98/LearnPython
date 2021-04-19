8782bcf47c13bd4db66ab909de8202d03dbd438e
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-19 12:33:43 -0600

    added tuples.py

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#tuples are similar to lists and
+#used to store multiple items in one variable
+#can't modify tuples like lists
+#IMMUTABLE
+
+coordinates = (4, 5)
+print(coordinates[0])
+
+coordinates1 = [(4,5),(5,6),(6,7),(7,8)] #list of tuples
+print(coordinates1[0])
