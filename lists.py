98e6b02c1e0c034cf0bb1e04a2ebe0fb24f3dc6f
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-19 12:01:44 -0600

    added lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+friends = ["John", "Morgan", "Justin", "Kyle", "Ritter"]
+
+print(friends)
+print(friends[0])
+print(friends[1])
+print(friends[-3])
+print(friends[-2])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:4])
+friends[0] = "Erik"
+print(friends)
+
