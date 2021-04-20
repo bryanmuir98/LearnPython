2791301bc5ebb83510a912dfffc705627600a593
Bryan Muir <bryanmuir@Bryans-MacBook-Pro-2.local>
2021-04-20 13:10:46 -0600

    added dictionary.py and while.py

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+print(monthConversions.get("Dec","Not a valid month key")) #Change "Dec" to "Luv"
+print(monthConversions["Nov"])
